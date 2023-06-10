8415ad4aee72ae5086ce18f2ca973ec34a740a19
Shees_Ali <sheessyed101@gmail.com>
2023-06-11 02:23:49 +0500

    changes made

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,16 @@
+"""Module providing Functions for translation"""
+from deep_translator import MyMemoryTranslator
+
+
+def english_to_french(english_text):
+    """Function to translate from english to french"""
+    french_text = MyMemoryTranslator(
+        source='en', target='fr').translate(english_text)
+    return french_text
+
+
+def french_to_english(french_text):
+    """Function to translate from french to english"""
+    english_text = MyMemoryTranslator(
+        source='fr', target='en').translate(french_text)
+    return english_text
